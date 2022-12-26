edeb5e0e8b4456e5c7c40309bd754b0eaa79eef7
TarunGupta10 <tarunhl1999@gmail.com>
2022-12-26 14:58:32 +0530

    commit

---
+++ Assignment-1/src/Game.ts
@@ -0,0 +1,8 @@
+import { Application } from 'pixi.js';
+import { gsap } from "gsap";
+export class Game extends Application {
+  constructor(opts: any) {
+    super(opts);
+   
+  }
+  }
---
+++ Assignment-1/src/index.ts
@@ -0,0 +1,12 @@
+import { Application } from 'pixi.js';
+import { Game } from './Game';
+    const gameDiv:HTMLDivElement = <HTMLDivElement>document.getElementById('game');
+    const app:Application = new Game({
+        // resizeTo: gameDiv,
+        width : innerWidth,
+        height : innerHeight,
+        backgroundColor: 0xFFE476,
+        sharedLoader: true,
+        sharedTicker: true
+    });
+    gameDiv.appendChild(app.view);
